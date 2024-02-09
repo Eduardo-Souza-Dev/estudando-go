36dfcef35767170edb6eddbe105b5070e8ae4566
Eduardo Souza <denis.souzadeoliveira05@gmail.com>
2024-02-08 22:04:51 -0300

    Insert no banco de dados

--- server/BD-connection.go
+++ server/BD-connection.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+//Primeira inserção ao banco
 
 func main() {
-	fmt.Println("Hello, World!")
+	db, err := sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/biblioteca")
+	if err != nil {
+		panic(err.Error())
+	}
+	//O defer é usado para fechar uma função, nesse caso ele encerra a conexão com o banco de dados
+	defer db.Close()
+	insert, err := db.Query("INSERT INTO livros (titulo)  VALUES ('Livro de Teste')")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer insert.Close()
+	fmt.Println("Livro inserido com sucesso!")
+
+	exportConnection(db)
+}
+
+func exportConnection(db *sql.DB) {
+	fmt.Println(db)
 }
